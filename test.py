0be904049a1ef5de7d884927dde0ddf141b5f6dc
Manisha Sahani <163099895+sizzling557@users.noreply.github.com>
2024-05-03 18:21:37 +0530

    Update test.py
    auto

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("Wassup Buddy!!")
+print("Amar is just mine")
+print("Amar loves me crazily")
+print("Amar is craving for my presence")
+print("Amar is happily & blissfully married with me")
+print("Amar thinks Manisha is the most beautiful girl in this entire world")
+
+
